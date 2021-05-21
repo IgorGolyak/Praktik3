9b1e0eca174fc26f73dbc1420071eaa2328d2063
Игорь Голяк <golyak.03@inbox.ru>
2021-05-21 10:44:21 +0700

    3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 2.
+            Console.WriteLine("Введите число x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = 0;
+            if (-2.4 < 5.7)
+            {
+                y = (Math.Pow(Math.Sin(x), 2));
+
+            }
+            else
+            {
+                Console.WriteLine("В противном случае 4");
+            }
+            Console.WriteLine("Ответ:" + y);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Практическая Работа №3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_Работа__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1.
+            Console.WriteLine("Введете число b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введете число a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int i = 100;
+            int a1 = i / 100;
+            int b1 = i / 10;
+            if (a > b)
+            {
+                Console.WriteLine("Первя цифра больше");
+            }
+            else
+            {
+                Console.WriteLine("Вторая цифра больше");
+            }
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+}
